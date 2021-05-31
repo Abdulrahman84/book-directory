7ff9f92923b995d92ae4c92a5a8dc7ae68b8ef76
Abdulrahman84 <55898084+Abdulrahman84@users.noreply.github.com>
2021-05-31 13:42:44 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const userRouter = require('./routers/user')
-const bookRouter = require('./routers/book')
+const userRouter = require("./routers/user");
+const bookRouter = require("./routers/book");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json())
+app.use(express.json());
 
-app.use(userRouter)
-app.use(bookRouter)
+app.use(cors());
+app.use(userRouter);
+app.use(bookRouter);
 
-app.use('/', (req, res) => {
-  res.send('<h1 style="text-align:center">Welcome to book directory</h1>')
-})
-app.use('/*', (req, res) => {
-    res.status(404).send({error: 'page not found'})
+app.use("/", (req, res) => {
+  res.send('<h1 style="text-align:center">Welcome to book directory</h1>');
+});
+app.use("/*", (req, res) => {
+  res.status(404).send({ error: "page not found" });
 });
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, (res, err) => {
-  app.listen(port)
-  console.log('connected ' + port)
-})
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+  (res, err) => {
+    app.listen(port);
+    console.log("connected " + port);
+  }
+);
