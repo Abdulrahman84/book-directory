cc4e312121d4f36222f1de4c2616dd4c24876e1f
Abdulrahman84 <55898084+Abdulrahman84@users.noreply.github.com>
2021-06-04 17:56:48 +0300

    cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(cors());
+app.options("*", cors());
 app.use(userRouter);
 app.use(bookRouter);
 
